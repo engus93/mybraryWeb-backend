064263a6fe6acd30a4726d2f01ef1172144a0472
Dudu <engus983@gmail.com>
2019-03-27 19:47:53 +0900

    ADD: app.js
    페이지 이동 가능 Express 공부해서 추가 하기...

--- code/app.js
+++ code/app.js
@@ -1,19 +1,27 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
-    }
-    if(request.url == '/favicon.ico'){
+    var _url = request.url;
+
+    console.log(url.parse(_url, true));
+
+    if(_url == '/favicon.ico'){
         response.writeHead(404);
-        response.end();
-        return;
-response.end();
-return;
+        response.end('404 Not found');
     }
+
+    if(_url == '/'){
+      _url = '/index.html';
+    } else if(_url == '/sign_up'){
+      _url = '/sign//sign_up.html';
+    } else if(_url == '/sign_in'){
+        _url = '/sign//sign_in.html';
+    }
+
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
- 
+    response.end(fs.readFileSync(__dirname + _url));
 });
+
 app.listen(3000);
