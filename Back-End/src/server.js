7109a4a6eef4d68dba9fc0649428735374dd0077
DuHyeon <engus983@gmail.com>
2019-06-12 14:30:15 +0900

    FEAT: Connect to graphql
    - Connected to graphql and query test

--- Back-End/src/server.js
+++ Back-End/src/server.js
@@ -1,1 +1,17 @@
-console.log("우왕ㅋ");
+// Import module
+import logger from "morgan";
+import { GraphQLServer } from "graphql-yoga";
+
+// My files list
+import "./env";
+import schema from "./schema";
+
+const PORT = process.env.PORT || 4000;
+const server = new GraphQLServer({ schema });
+
+// morgan dev mode => log check module
+server.express.use(logger("dev"));
+
+server.start({ port: PORT }, () => {
+  console.log(`✅　Server running on port http://localhost:${PORT}`);
+});
